116a94b0d5088f5e0a6b475bf06e2f7bc561a8f5
dxl0 <dylan1122@outlook.fr>
2023-06-16 10:37:20 +0200

    comment main.c

--- main.c
+++ main.c
@@ -1,18 +1,26 @@
-#include"p18f2520.h"
-#include"fonctionnement.h"
-#include"init.h"
-#include"IT.h"
-#include<stdio.h>
+#include "p18f2520.h"
+#include "fonctionnement.h"
+#include "init.h"
+#include "IT.h"
+#include <stdio.h>
 
 #pragma config OSC = INTIO67
 #pragma config PBADEN = OFF, WDT = OFF, LVP = OFF, DEBUG = ON
 
-
-
-void main(void){
+void main(void)
+{
+    /*
+    initialise tout les périphériques
+    */
     init_all();
-    while(1){
-        if(PORTBbits.RB5) fonctionnement();
-        else stop();
+    /*
+    si la led RB5 est allumé, le robot est en fonctionnement, sinon il s'arrête
+    */
+    while (1)
+    {
+        if (PORTBbits.RB5)
+            fonctionnement();
+        else
+            stop();
     }
 }
