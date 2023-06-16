8cf9ba708de484fecf068a75ae96f057e1cb04d0
dxl0 <dylan1122@outlook.fr>
2023-06-16 10:48:25 +0200

    change printf

--- IT.c
+++ IT.c
@@ -1,24 +1,23 @@
-#include"p18f2520.h"
-#include"IT.h"
-#include"fonctionnement.h"
-#include"init.h"
-#include"MI2C.h"
+#include "p18f2520.h"
+#include "IT.h"
+#include "fonctionnement.h"
+#include "init.h"
+#include "MI2C.h"
 
-#pragma code HighVector=0x08
+#pragma code HighVector = 0x08
 
-unsigned float V_SEUIL = 2.5 ;
+unsigned float V_SEUIL = 2.5;
 
-int etat = 0, temps_tourne =0, i, nombre_mes =0, temps_bat=0, compteur=0;
+int etat = 0, temps_tourne = 0, i, nombre_mes = 0, temps_bat = 0, compteur = 0;
 unsigned char distance;
 unsigned int sum_mes, UBAT;
-int tourne =1;
+int tourne = 1;
 char Recv_Buff[3];
 
 void IntHighVector(void)
 {
-    _asm
-        goto HighISR
-    _endasm
+    _asm goto HighISR
+        _endasm
 }
 #pragma code
 
@@ -26,72 +25,74 @@ void IntHighVector(void)
 
 void HighISR(void)
 {
-    if(INTCONbits.INT0IF){
+    if (INTCONbits.INT0IF)
+    {
         Lire_i2c_Telecom(0xA2, Recv_Buff);
-        if(Recv_Buff[1] == 0x33){
-            PORTBbits.RB5=1;
+        if (Recv_Buff[1] == 0x33)
+        {
+            PORTBbits.RB5 = 1;
         }
-        temps_tourne=0;
-        etat=0;
-        temps_bat=0;
+        temps_tourne = 0;
+        etat = 0;
+        temps_bat = 0;
         INTCONbits.INT0IF = 0;
     }
 
-    if (INTCONbits.TMR0IF && PORTBbits.RB5 )
+    if (INTCONbits.TMR0IF && PORTBbits.RB5)
     {
 
-        //blink(); //LED for timer
+        // blink(); //LED for timer
         temps_tourne++;
         temps_bat++;
-        distance = mesurer_distance(); //distance between the robot and the obstacle
+        distance = mesurer_distance(); // distance between the robot and the obstacle
         LED_print(distance);
-        printf("%x\r", distance);
-        if (temps_tourne == 70 && etat!=5) //motors every 3.5s (=90*50*10-3)
+        if (temps_tourne == 70 && etat != 5) // motors every 3.5s (=90*50*10-3)
         {
-            etat++; //state of the robot
-            printf("Etat %d\r",etat);
-            if (etat== 4)
+            etat++; // state of the robot
+            printf("Etat %d\r", etat);
+            if (etat == 4)
             {
-                etat= 0;
+                etat = 0;
             }
             temps_tourne = 0;
         }
 
-        if(temps_bat == 20) //check_battery every 1s (50*10-3*20)
+        if (temps_bat == 20) // check_battery every 1s (50*10-3*20)
         {
             UBAT = (int)etat_bat();
-            if(nombre_mes < 4)
+            if (nombre_mes < 4)
             {
                 nombre_mes++;
-                sum_mes+= UBAT;
+                sum_mes += UBAT;
             }
             else
             {
-                if(sum_mes/4 < (255/5)*V_SEUIL)
+                if (sum_mes / 4 < (255 / 5) * V_SEUIL)
                 {
-                    printf("\r\rBatterie faible\r\r"); //avg tension of the battery
+                    printf("\n\r\n\rBatterie faible\n\r\n\r"); // avg tension of the battery
                     PORTBbits.RB5 = 0;
                 }
-                printf("Vbat_moyen = %d mV\r",(sum_mes/4)*19);
+                printf("Vbat_moyen = %d mV\n\r", (sum_mes / 4) * 19);
                 sum_mes = 0;
                 nombre_mes = 0;
             }
 
-            temps_bat=0;
+            temps_bat = 0;
         }
 
-        if(distance>0x12)
+        if (distance > 0x12)
         {
-            printf("avancer tout droit");
+            printf("avancer tout droit\n\r");
             etat = 5;
         }
-        if(etat==5 && compteur == 68)
+        if (etat == 5 && compteur == 68)
         {
-            PORTBbits.RB5=0;
-            compteur=0;
+            PORTBbits.RB5 = 0;
+            compteur = 0;
         }
-        if(etat==5) compteur++;
+        if (etat == 5)
+            compteur++;
 
-        INTCONbits.TMR0IF = 0; //IT flag
+        INTCONbits.TMR0IF = 0; // IT flag
     }
 }
